1ad43d58ccd332410ce42292a4a1d5ffe2bbede5
RitaButusova <47254480+RitaButusova@users.noreply.github.com>
2020-09-27 19:40:23 +0300

    Initial commit

---
+++ auto-write-text/script.js
@@ -0,0 +1,13 @@
+const text = 'my heart is making meow meow'
+
+let index = 0;
+
+function writeText() {
+    document.body.innerText = text.slice(0, index);
+    index++;
+    if (index > text.length){
+        index = 0;
+    }
+}
+
+setInterval(writeText, 100);
